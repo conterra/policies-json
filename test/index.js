5d00baad7d798a0457bd6851323f0f355ab047ef
mre <m.reiprecht@conterra.de>
2020-09-10 11:35:59 +0200

    Add further tests and restrictions

--- test/index.js
+++ test/index.js
@@ -12,7 +12,13 @@ describe("policies.schema.json", function () {
         assert.isTrue(validate(require("./json/properties.json")));
     });
     it("should not accept a properties section with number values", function () {
-        assert.isFalse(validate(require("./json/properties-invalid-numbers.json")));
+        assert.isFalse(validate(require("./json/properties-invalid-number-values.json")));
+    });
+    it("should not accept a properties section with keys starting with number values", function () {
+        assert.isFalse(validate(require("./json/properties-invalid-starting-number.json")));
+    });
+    it("should not accept a properties section with keys using invalid keys", function () {
+        assert.isFalse(validate(require("./json/properties-invalid-character-in-key.json")));
     });
     it("should not accept a missing restriction type", function () {
         assert.isFalse(validate(require("./json/restrictions-missing-type.json")));
@@ -28,6 +34,9 @@ describe("policies.schema.json", function () {
     });
     it("should accept a spatial restriction", function () {
         assert.isTrue(validate(require("./json/restrictions-spatial.json")));
+    });
+    it("should accept a spatial restriction without operation field", function () {
+        assert.isTrue(validate(require("./json/restrictions-spatial-operation-optional.json")));
     });
     it("should not accept a spatial restriction with unknown property", function () {
         assert.isFalse(validate(require("./json/restrictions-spatial-unknown-property.json")));
@@ -55,5 +64,32 @@ describe("policies.schema.json", function () {
     });
     it("should accept a simple policy with layer and role", function () {
         assert.isTrue(validate(require("./json/policies.json")));
+    });
+    it("should not accept a policy with empty layer value", function () {
+        assert.isFalse(validate(require("./json/policies-layers-no-empty-value.json")));
+    });
+    it("should not accept a policy with empty layer array", function () {
+        assert.isFalse(validate(require("./json/policies-layers-no-empty-array.json")));
+    });
+    it("should not accept a policy with empty role value", function () {
+        assert.isFalse(validate(require("./json/policies-roles-no-empty-value.json")));
+    });
+    it("should not accept a policy with empty role array", function () {
+        assert.isFalse(validate(require("./json/policies-roles-no-empty-array.json")));
+    });
+    it("should not accept a policy with missing roles property", function () {
+        assert.isFalse(validate(require("./json/policies-roles-required.json")));
+    });
+    it("should not accept a policy with missing layers property", function () {
+        assert.isFalse(validate(require("./json/policies-layers-required.json")));
+    });
+    it("should accept a policy with empty restrictions array", function () {
+        assert.isTrue(validate(require("./json/policies-restrictions-can-be-empty-array.json")));
+    });
+    it("should not accept a policy with empty restrictions value", function () {
+        assert.isFalse(validate(require("./json/policies-restrictions-no-empty-value.json")));
+    });
+    it("should accept a policy using ${key} expressions", function () {
+        assert.isTrue(validate(require("./json/policies-property-replacement-allowed.json")));
     });
 });
